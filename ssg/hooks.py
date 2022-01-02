353ec8ef0a47bc2e4240e43cc29306364dd7fc3a
p-a <peter.augustinak@gmail.com>
2022-01-02 16:43:17 +0100

    Module2 first draft

---
+++ ssg/hooks.py
@@ -0,0 +1,27 @@
+_callbacks = {}
+
+
+def register(hook, order=0):
+    def register_callback(func):
+        """
+        responsible for adding default key value pairs to the _callbacks
+        dictionary.
+        """
+        _callbacks.setdefault(hook, {}).setdefault(order, []).append(func)
+        return func
+    return register_callback
+
+
+def event(hook, *args):
+    """Fires an aevent."""
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            func(*args)
+
+
+def filter(hook, value, *args):
+    """Capture the result of the callback."""
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            value = func(value, *args)
+    return value
