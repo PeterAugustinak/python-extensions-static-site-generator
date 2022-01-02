844b0f3e350176c81f571b16090b7433c9c5d828
p-a <peter.augustinak@gmail.com>
2022-01-02 15:57:58 +0100

    Module1 first draft

---
+++ ssg/extensions.py
@@ -0,0 +1,24 @@
+import sys
+import importlib
+from pathlib import Path
+
+
+def load_module(directory, name):
+    """Loads module called 'name' from 'directory' folder."""
+    sys.path.insert(0, directory)
+    importlib.import_module(name)
+    sys.path.pop(0)
+
+
+def load_directory(directory):
+    """Loads entire directory of extensions."""
+    for path in Path(directory).rglob('.py'):
+        load_module(directory.as_posix(), path.stem)
+
+
+def load_bundled():
+    """Loads bundled extensions."""
+    directory = Path(__file__).parent / "extensions"
+    load_directory(directory)
+
+
